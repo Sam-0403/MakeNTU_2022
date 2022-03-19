d69f42ad5f00ed7ae6bfae83ceb96108c94f375f
鄭至盛 <sam22187212@gmail.com>
2022-03-20 05:47:26 +0800

    Try UART in GUI

--- hardware/main.py
+++ hardware/main.py
@@ -51,7 +51,7 @@ class ClassificationApp(App):
         sleep(2)
         # self.ai_model.image_loader("S__10313763.jpg")
         # self.ai_model.image_loader(r"/media/pi/_s_W_Ma_/example.jpg")
-        os.chdir('/media/pi/_s_W_Ma_1')
+        os.chdir('/media/pi/_s_W_Ma_1/')
         self.ai_model.image_loader(os.path.abspath('example.jpg'))
         # print(os.getcwd())
 
