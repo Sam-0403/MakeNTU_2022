719115381bffeb47295b48b262962dacf72b4311
鄭至盛 <sam22187212@gmail.com>
2022-03-20 04:31:29 +0800

    Try UART in GUI

--- hardware/get_uart.py
+++ hardware/get_uart.py
@@ -1,80 +1,92 @@
-import serial
-from time import sleep, time
-import io
-import PIL.Image as Image
+# import serial
+# from time import sleep, time
+# import io
+# import PIL.Image as Image
 
-class UART_controller():
-    ser = None
-    def __init__(self):
-        # self.ser = serial.Serial ("/dev/ttyAMA0", 115200, timeout=10)    #Open port with baud rate
-        self.ser = serial.Serial ("/dev/ttyAMA0", 115200)
-        self.ser.write(b'S')
-        print("Write")
+# class UART_controller():
+#     ser = None
+#     def __init__(self):
+#         # self.ser = serial.Serial ("/dev/ttyAMA0", 115200, timeout=10)    #Open port with baud rate
+#         self.ser = serial.Serial ("/dev/ttyAMA0", 115200)
+#         self.ser.write(b'S')
+#         print("Write")
     
-    def receive(self):
-        received_data = self.ser.read()              #read serial port
-        sleep(0.03)
-        data_left = self.ser.inWaiting()             #check for remaining byte
-        received_data += self.ser.read(data_left)
-        return received_data
+#     def receive(self):
+#         received_data = self.ser.read()              #read serial port
+#         sleep(0.03)
+#         data_left = self.ser.inWaiting()             #check for remaining byte
+#         received_data += self.ser.read(data_left)
+#         return received_data
 
-    def receive_time(self, message):
-        print("Writing Data!")
-        self.ser.write(message)
-        print("Receiving Data!")
+#     def receive_time(self, message):
+#         print("Writing Data!")
+#         self.ser.write(message)
+#         print("Receiving Data!")
 
-        # received_data = self.ser.read()
-        # sleep(0.03)
-        # data_left = self.ser.inWaiting()             #check for remaining byte
-        # received_data += self.ser.read(data_left)
-        # print("First Receive")
-        # while time()<time_end:
-        #     print("Looping")
-        #     received_data += self.ser.read()              #read serial port
-        #     sleep(0.03)
-        #     data_left = self.ser.inWaiting()             #check for remaining byte
-        #     received_data += self.ser.read(data_left)
-        # print("Receiving Data Finish!")
-        sleep(10)
-        received_data = self.ser.read_all()
-        print(len(received_data))
-        print("Receiving Data Finish!")
-        try:
-            image = Image.open(io.BytesIO(received_data))
-            image.save('temp.jpg')
-            print("Save Success")
-        except:
-            print("Save Error")
-        # return received_data
+#         # received_data = self.ser.read()
+#         # sleep(0.03)
+#         # data_left = self.ser.inWaiting()             #check for remaining byte
+#         # received_data += self.ser.read(data_left)
+#         # print("First Receive")
+#         # while time()<time_end:
+#         #     print("Looping")
+#         #     received_data += self.ser.read()              #read serial port
+#         #     sleep(0.03)
+#         #     data_left = self.ser.inWaiting()             #check for remaining byte
+#         #     received_data += self.ser.read(data_left)
+#         # print("Receiving Data Finish!")
+#         sleep(10)
+#         received_data = self.ser.read_all()
+#         print(len(received_data))
+#         print("Receiving Data Finish!")
+#         try:
+#             image = Image.open(io.BytesIO(received_data))
+#             image.save('temp.jpg')
+#             print("Save Success")
+#         except:
+#             print("Save Error")
+#         # return received_data
 
-    def write(self, message):
-        self.ser.write(message)
+#     def write(self, message):
+#         self.ser.write(message)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    # ser = serial.Serial ("/dev/ttyAMA0", 115200)
-    # ser = serial.Serial ("/dev/ttyAMA0", 115200, timeout=10)    #Open port with baud rate
-    ser = UART_controller()
-    print("Configure UART Success!")
-    # ser.write(b'S')
-    # ser.write(b'S')
+#     # ser = serial.Serial ("/dev/ttyAMA0", 115200)
+#     # ser = serial.Serial ("/dev/ttyAMA0", 115200, timeout=10)    #Open port with baud rate
+#     ser = UART_controller()
+#     print("Configure UART Success!")
+#     # ser.write(b'S')
+#     # ser.write(b'S')
 
-    # sleep(5)
-    # received_data = ser.read_all()
-    # print(len(received_data))
-    # print("Receiving Data Finish!")
+#     # sleep(5)
+#     # received_data = ser.read_all()
+#     # print(len(received_data))
+#     # print("Receiving Data Finish!")
 
-    # sleep(30)
-    # received_data = ser.read_all()
-    # print (received_data)
+#     # sleep(30)
+#     # received_data = ser.read_all()
+#     # print (received_data)
 
-    while True:
-        # received_data = ser.read(153600)              #read serial port
-        # ser.write(b'S')
-        received_data = ser.ser.read()
-        sleep(0.03)
-        data_left = ser.ser.inWaiting()             #check for remaining byte
-        received_data += ser.ser.read(data_left)
-        print (received_data)                   #print received data
-        # ser.write(received_data)                #transmit data serially 
+#     while True:
+#         # received_data = ser.read(153600)              #read serial port
+#         # ser.write(b'S')
+#         received_data = ser.ser.read()
+#         sleep(0.03)
+#         data_left = ser.ser.inWaiting()             #check for remaining byte
+#         received_data += ser.ser.read(data_left)
+#         print (received_data)                   #print received data
+#         # ser.write(received_data)                #transmit data serially 
+import serial
+from time import sleep
+
+ser = serial.Serial ("/dev/ttyAMA0", 115200)    #Open port with baud rate
+ser.write(b'S')
+while True:
+    received_data = ser.read()              #read serial port
+    sleep(0.03)
+    data_left = ser.inWaiting()             #check for remaining byte
+    received_data += ser.read(data_left)
+    print (received_data)                   #print received data
+    ser.write(received_data)                #transmit data serially 
