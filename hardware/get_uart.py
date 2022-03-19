2d4656b0d15271e27b6c592b7119545b727ea861
鄭至盛 <sam22187212@gmail.com>
2022-03-19 16:21:00 +0800

    Add UART

--- hardware/get_uart.py
+++ hardware/get_uart.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
 
     ser = serial.Serial ("/dev/ttyS0", 115200)    #Open port with baud rate
     while True:
-        # received_data = ser.read()              #read serial port
+        received_data = ser.read()              #read serial port
         sleep(0.03)
-        # data_left = ser.inWaiting()             #check for remaining byte
-        # received_data += ser.read(data_left)
-        print (ser.read())                   #print received data
+        data_left = ser.inWaiting()             #check for remaining byte
+        received_data += ser.read(data_left)
+        print (received_data)                   #print received data
         # ser.write(received_data)                #transmit data serially 
