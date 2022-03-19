71834c300d5ce78c38dac396c6904be81859bcc8
鄭至盛 <sam22187212@gmail.com>
2022-03-19 23:18:18 +0800

    Try RFID

--- hardware/classify.py
+++ hardware/classify.py
@@ -108,9 +108,9 @@ class Decide():
         return a
     
 
-dd = Decide("../0319_best.pth")
-dd.image_loader("../S_10313763.jpg")
+dd = Decide("/0319_best.pth")
+dd.image_loader("/S_10313763.jpg")
 
 if __name__ == "__main__":
-    dd = Decide("../0319_best.pth")
-    dd.image_loader("../S_10313763.jpg")
+    dd = Decide("/0319_best.pth")
+    dd.image_loader("/S_10313763.jpg")
