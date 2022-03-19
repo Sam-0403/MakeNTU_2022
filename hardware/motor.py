2136cdb13b6f162c56c27dcb2788547905ee4af7
鄭至盛 <sam22187212@gmail.com>
2022-03-20 07:33:48 +0800

    Try UART in GUI

--- hardware/motor.py
+++ hardware/motor.py
@@ -1,68 +1,37 @@
 import RPi.GPIO as GPIO
-from time import sleep
 
-in1 = 24
-in2 = 23
-en = 25
-temp1 = 1
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
-GPIO.output(in1, GPIO.LOW)
-GPIO.output(in2, GPIO.LOW)
-p = GPIO.PWM(en, 1000)
-p.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")
-while(1):
-    x = input("Type:")
-    if x == 'r':
-        print("run")
-        if(temp1 == 1):
-            GPIO.output(in1, GPIO.HIGH)
-            GPIO.output(in2, GPIO.LOW)
-            print("forward")
-            x = 'z'
-        else:
-            GPIO.output(in1, GPIO.LOW)
-            GPIO.output(in2, GPIO.HIGH)
-            print("backward")
-            x = 'z'
-    elif x == 's':
-        print("stop")
-        GPIO.output(in1, GPIO.LOW)
-        GPIO.output(in2, GPIO.LOW)
-        x = 'z'
-    elif x == 'f':
-        print("forward")
-        GPIO.output(in1, GPIO.HIGH)
-        GPIO.output(in2, GPIO.LOW)
-        temp1 = 1
-        x = 'z'
-    elif x == 'b':
-        print("backward")
-        GPIO.output(in1, GPIO.LOW)
-        GPIO.output(in2, GPIO.HIGH)
-        temp1 = 0
-        x = 'z'
-    elif x == 'l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        x = 'z'
-    elif x == 'm':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        x = 'z'
-    elif x == 'h':
-        print("high")
-        p.ChangeDutyCycle(75)
-        x = 'z'
-    elif x == 'e':
-        GPIO.cleanup()
-        break
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
+
+while True:
+    ch = input("車子如何移動 f往前  b往後 l往左  r往右 q結束？")
+    if ch == 'q':
+       GPIO.output(17, False)
+       GPIO.output(18, False)
+       GPIO.output(22, False)
+       GPIO.output(23, False)
+       break
+    if ch == 'f':
+       GPIO.output(17, False)
+       GPIO.output(18, True)
+       GPIO.output(22, False)
+       GPIO.output(23, True)
+    if ch == 'b':
+       GPIO.output(17, True)
+       GPIO.output(18, False)
+       GPIO.output(22, True)
+       GPIO.output(23, False)
+    if ch == 'r':
+       GPIO.output(17, False)
+       GPIO.output(18, True)
+       GPIO.output(22, False)
+       GPIO.output(23, False)
+    if ch == 'l':
+       GPIO.output(17, False)
+       GPIO.output(18, False)
+       GPIO.output(22, False)
+       GPIO.output(23, True)
