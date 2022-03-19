4c6d6f68c085262c6ee89e54ce18fb3adbebdf21
鄭至盛 <sam22187212@gmail.com>
2022-03-20 05:02:17 +0800

    Try UART in GUI

--- hardware/main.py
+++ hardware/main.py
@@ -51,7 +51,8 @@ class ClassificationApp(App):
         sleep(2)
         # self.ai_model.image_loader("S__10313763.jpg")
         # self.ai_model.image_loader(r"/media/pi/_s_W_Ma_/example.jpg")
-        self.ai_model.image_loader(os.path.join('/media/pi/_s_W_Ma_',os.sep, 'example.jpg'))
+        os.chdir(r'/media/pi/_s_W_Ma_')
+        self.ai_model.image_loader('example.jpg')
 
     def switch_to_login(self):
         self.sm.current = "LoginScreen"
