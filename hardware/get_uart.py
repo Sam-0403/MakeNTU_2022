bb6cb9041d4f9887fec280db48dd39c940e3e7a7
鄭至盛 <sam22187212@gmail.com>
2022-03-20 02:42:19 +0800

    Try UART in GUI

--- hardware/get_uart.py
+++ hardware/get_uart.py
@@ -61,9 +61,9 @@ if __name__ == "__main__":
     # print("Receiving Data Finish!")
 
     while True:
-        received_data = ser.read()              #read serial port
-        sleep(0.03)
-        data_left = ser.inWaiting()             #check for remaining byte
-        received_data += ser.read(data_left)
+        received_data = ser.read(153600)              #read serial port
+        # sleep(0.03)
+        # data_left = ser.inWaiting()             #check for remaining byte
+        # received_data += ser.read(data_left)
         print (received_data)                   #print received data
         # ser.write(received_data)                #transmit data serially 
